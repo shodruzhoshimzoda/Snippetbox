f2b0b827f152ab047bc9813c8bc7488a868a6c74
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-18 12:50:47 +0500

    Was included arrays of snippets

--- cmd/web/template.go
+++ cmd/web/template.go
@@ -3,5 +3,6 @@ package main
 import "github.com/shodruzhoshimzoda/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
