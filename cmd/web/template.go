c9a1ea7d987dfa3bba29ec97cc307deec772562e
sabrrise <shodruzxoshimzoda@gmail.com>
2026-07-19 22:15:44 +0500

    Was implemented function for caching templates

--- cmd/web/template.go
+++ cmd/web/template.go
@@ -1,8 +1,43 @@
 package main
 
-import "github.com/shodruzhoshimzoda/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/shodruzhoshimzoda/snippetbox/internal/models"
+)
 
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+
+	// initialize new map to act cache
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	// Loop through the page filepaths one-by-on
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/partials/nav.html",
+			page,
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+
+	}
+	return cache, nil
 }
