2bc4ac7a059c240a1a7c2f3fbf7da5004d082771
sabrrise <shodruzxoshimzoda@gmail.com>
2026-08-02 23:04:44 +0500

    Panic recovery

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,6 +16,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreate)
 
-	return app.logRequestMiddleware(commonHeaders(mux))
+	return app.recoverPanic(app.logRequestMiddleware(commonHeaders(mux)))
 
 }
