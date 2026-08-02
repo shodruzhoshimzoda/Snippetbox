4ecf5de208e70bb0ff57753a56a85184d92ab5c1
sabrrise <shodruzxoshimzoda@gmail.com>
2026-08-02 22:33:45 +0500

    Was wrapped handlers with our middleware for commonHeaders

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,9 +2,6 @@ package main
 
 import "net/http"
 
-
-
-
 func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
@@ -12,16 +9,13 @@ func (app *application) routes() http.Handler {
 	// serve static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	
-	mux.Handle("GET /static/", http.StripPrefix("/static",fileServer))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	// routes registration
 	mux.HandleFunc("/{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("POST /snippet/create",app. snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreate)
 
-	
-	return mux
-	
+	return commonHeaders(mux)
 
 }
